7ffab521206014e5f8dedc940e7e16fdc4ac9477
uzi harband <uzi.harband@gmail.com>
2020-11-16 07:39:31 +0200

    jitter logging

--- pkg/rtpbuffer/jitter.go
+++ pkg/rtpbuffer/jitter.go
@@ -6,6 +6,7 @@ import (
 	"github.com/pion/rtp"
 	"github.com/pion/webrtc/v3"
 	"github.com/pion/webrtc/v3/pkg/media"
+	log "github.com/sirupsen/logrus"
 	"sync"
 	"time"
 )
@@ -14,17 +15,23 @@ type Jitter struct {
 	buffer map[uint16]*rtp.Packet
 	peerConnection *webrtc.PeerConnection
 	track *webrtc.Track
+	segmentStart uint16
+	segmentEnd uint16
+	currentSN uint16
+	mapSync sync.RWMutex
 	stop  bool
 }
 
-var segmentStart = uint16(0)
-var segmentEnd = uint16(0)
-var currentSN = uint16(0)
-var mapSync = sync.RWMutex{}
-
-
 func NewJitter(pc *webrtc.PeerConnection, t *webrtc.Track) *Jitter {
-	jitter := &Jitter{buffer: make(map[uint16]*rtp.Packet), peerConnection: pc, track: t, stop: false}
+	jitter := &Jitter{
+		buffer: make(map[uint16]*rtp.Packet),
+		peerConnection: pc,
+		track: t,
+		segmentStart: uint16(0),
+		segmentEnd: uint16(0),
+		currentSN: uint16(0),
+		mapSync: sync.RWMutex{},
+		stop: false}
 	jitter.startRTCP()
 	return  jitter
 }
@@ -34,6 +41,7 @@ func (j *Jitter) Close(){
 	for k := range j.buffer{
 		delete(j.buffer, k)
 	}
+	j.buffer = nil
 }
 
 func (j *Jitter) startRTCP(){
@@ -49,10 +57,19 @@ func (j *Jitter) startRTCP(){
 				return
 			}
 			packets, _ := sender.ReadRTCP()
-			//fmt.Println("number of packets read: ", len(packets))
 
 			for _, packet := range packets{
-				//fmt.Println("packet type: ", reflect.TypeOf(packet))
+				/*
+				log.WithFields(
+					log.Fields{
+						"component": "jitter",
+						"segmentStart": segmentStart,
+						"segmentEnd": segmentEnd,
+						"size": len(j.buffer),
+					}).Info("rtcp: got ", reflect.TypeOf(packet), " packet")
+
+				 */
+
 				switch packet := packet.(type) {
 				case *rtcp.PictureLossIndication:
 					//fmt.Println("got pli")
@@ -65,9 +82,9 @@ func (j *Jitter) startRTCP(){
 					for _, nack := range nack.Nacks{
 						nackCount++
 						//fmt.Println("nackCount: ", nackCount, ". got nack for packet ", nack.PacketID)
-						mapSync.Lock()
+						j.mapSync.Lock()
 						j.track.WriteRTP(j.buffer[nack.PacketID])
-						mapSync.Unlock()
+						j.mapSync.Unlock()
 					}
 				default:
 				}
@@ -81,22 +98,35 @@ func (j *Jitter) startRTCP(){
 			if j.stop == true{
 				return
 			}
-			//fmt.Println("buffer size: ", len(j.buffer))
-			if segmentEnd == 0{
-				segmentEnd = currentSN
+
+			if j.segmentEnd == 0{
+				j.segmentEnd = j.currentSN
+				log.WithFields(
+					log.Fields{
+						"component": "jitter",
+						"segmentStart": j.segmentStart,
+						"segmentEnd": j.segmentEnd,
+						"size": len(j.buffer),
+					}).Info("cleanup: first cleanup. setting segmentEnd")
 			}else{
-				//fmt.Println("segments ", segmentStart, " ", segmentEnd, " removing ", segmentEnd - segmentStart)
+				log.WithFields(
+					log.Fields{
+						"component": "jitter",
+						"segmentStart": j.segmentStart,
+						"segmentEnd": j.segmentEnd,
+						"size": len(j.buffer),
+					}).Info("cleanup. about to delete ", j.segmentEnd - j.segmentStart, " entries")
+
 				// clear previous segment
-				for i := segmentStart; i<=segmentEnd; i++{
+				for i := j.segmentStart; i<=j.segmentEnd; i++{
 					if _, ok := j.buffer[i]; ok {
-						mapSync.Lock()
+						j.mapSync.Lock()
 						delete(j.buffer, i)
-						mapSync.Unlock()
+						j.mapSync.Unlock()
 					}
 				}
-				segmentStart = segmentEnd + 1
-				segmentEnd = currentSN
-				//fmt.Println("after assigning. start ", segmentStart, " end: ", segmentEnd, ". size after delete ", len(j.buffer))
+				j.segmentStart = j.segmentEnd + 1
+				j.segmentEnd = j.currentSN
 			}
 		}
 	}()
@@ -111,14 +141,20 @@ func (j *Jitter) WriteSample(s media.Sample) error {
 
 	// prefer sending packets over cleaning packets
 	for _, p := range packets {
-		if segmentStart == 0{
-			segmentStart = p.Header.SequenceNumber
+		if j.segmentStart == 0{
+			j.segmentStart = p.Header.SequenceNumber
+			log.WithFields(
+				log.Fields{
+					"component": "jitter",
+					"segmentStart": j.segmentStart,
+					"segmentEnd": j.segmentEnd,
+					"size": len(j.buffer),
+				}).Info("first packet for connection")
 		}
-		currentSN = p.Header.SequenceNumber
-		//fmt.Println("current sn ", currentSN)
-		mapSync.Lock()
+		j.currentSN = p.Header.SequenceNumber
+		j.mapSync.Lock()
 		j.buffer[p.Header.SequenceNumber] = p
-		mapSync.Unlock()
+		j.mapSync.Unlock()
 		err := j.track.WriteRTP(p)
 		if err != nil {
 			return err
